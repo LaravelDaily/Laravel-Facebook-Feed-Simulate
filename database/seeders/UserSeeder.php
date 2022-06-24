bea719aa6ce2758be86862711d4718d4e537548b
Nerijus <nerijus.bukartas@gmail.com>
2022-06-24 13:57:41 +0300

    Init

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::factory(10)->create();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+
+Route::middleware('auth')->group(function () {
+    Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
+    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+});
+
+require __DIR__.'/auth.php';
