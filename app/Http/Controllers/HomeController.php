e52c13db9d5af15da70677860199a28f7f62504f
Nerijus <nerijus.bukartas@gmail.com>
2022-07-05 16:48:06 +0300

    Order posts by engagement

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use Maize\Markable\Models\Reaction;
+
 class HomeController extends Controller
 {
     public function __invoke()
     {
         $posts = auth()->user()->followersPosts()
-            ->with(['media', 'user.media', 'reactions', 'popularComment.popularReply'])
-            ->withCount(['reactions', 'comments', 'sharedPost'])
+            ->with([
+                'media',
+                'user' => [
+                    'media'
+                ],
+                'reactions',
+                'popularComment' => [
+                    'popularReply'
+                ]
+            ])
+            ->withCount(['media', 'reactions', 'comments', 'sharedPost'])
+            ->addSelect(['post_comments_reactions_count' =>
+                             Reaction::query()->selectRaw('count(markable_reactions.id)')
+                                 ->where('markable_reactions.markable_type', '=', 'App\\Models\\PostComment')
+                                 ->whereColumn('markable_reactions.markable_id', '=','post_comments.id')
+                                 ->join('post_comments', 'posts.id', '=', 'post_comments.post_id')
+            ])
             ->whereDate('posts.created_at', '>=', now()->subDays(7))
+            ->orderByDesc(
+                DB::raw('reactions_count + (shared_post_count * 3) + (comments_count * 2) + post_comments_reactions_count + (CASE WHEN media_count > 0 THEN 10 ELSE 0 END)')
+            )
+            ->take(10)
             ->get();
-
-//        dd($posts);
 
         return view('home', compact('posts'));
     }
